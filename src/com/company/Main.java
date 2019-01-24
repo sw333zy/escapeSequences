b39805ca0b127cf7e3613ca2e393ecab4cf562af
sw333zy <sw333zy@gmail.com>
2019-01-24 11:28:34 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("********");
+	System.out.print("\n");
+	System.out.print("*      *");
+	System.out.print("\n");
+	System.out.print("*      *");
+	System.out.print("\n");
+	System.out.print("*      *");
+	System.out.print("\n");
+	System.out.print("*      *");
+	System.out.print("\n");
+	System.out.print("********");
+    }
+}
